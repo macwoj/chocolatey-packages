1f0b4baa68ecb6eb691e39bc5e0a80c923863583
macwoj <maciej.wojton@gmail.com>
2017-10-04 12:44:40 -0400

    changing to sudo in script

--- vim-solarized/tools/install_vim.sh
+++ vim-solarized/tools/install_vim.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-apt-get -y install software-properties-common
-apt-get -y install python-dev python-pip python3-dev python3-pip
-apt-get -y install ctags vim-doc vim-scripts indent
-add-apt-repository ppa:neovim-ppa/stable
-add-apt-repository ppa:jonathonf/vim
-apt update
-apt-get -y install neovim
-apt-get -y install vim
+sudo apt-get -y install software-properties-common
+sudo apt-get -y install python-dev python-pip python3-dev python3-pip
+sudo apt-get -y install ctags vim-doc vim-scripts indent
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt-get -y install neovim
+sudo apt-get -y install vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \

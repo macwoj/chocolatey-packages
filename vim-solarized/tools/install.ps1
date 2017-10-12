abbefc19e80868ab842ca84c2104d3dd3e02e245
macwoj <maciej.wojton@gmail.com>
2017-10-12 12:35:21 -0400

    adding putty font install

--- vim-solarized/tools/install.ps1
+++ vim-solarized/tools/install.ps1
@@ -2,13 +2,14 @@ choco install git -y
 choco install vim -y
 choco install putty -y
 git clone https://github.com/powerline/fonts.git
-./fonts/install.ps1 DejaVuSansMono
+./fonts/install.ps1 DejaVu*
 git clone https://github.com/neilpa/cmd-colors-solarized.git
 ./cmd-colors-solarized/Update-Link.ps1 "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Bash on Ubuntu on Windows.lnk" light
 ./cmd-colors-solarized/Update-Link.ps1 "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows PowerShell.lnk" light
 regedit /s ./cmd-colors-solarized/solarized-light.reg
 git clone https://github.com/altercation/solarized.git
 regedit /s ./solarized/putty-colors-solarized/solarized_light.reg
+regedit /s ./putty_font.reg
 git clone https://github.com/macwoj/dotfiles.git
 copy ./dotfiles/.vimrc ~\_vimrc
 md ~\vimfiles\autoload
@@ -19,5 +20,5 @@ $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     "~\vimfiles\autoload\plug.vim"
   )
 )
-"C:\Program Files (x86)\Vim\vim80\gvim.exe" -c ":PlugInstall" -c ":q" -c ":q"
+& 'C:\Program Files (x86)\Vim\vim80\gvim.exe' -c ":PlugInstall" -c ":q" -c ":q"
 bash -c "installnvim.sh"
